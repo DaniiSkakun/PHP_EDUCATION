43e0c99b4b9a03708232a22ce47fba75c191496f
Danichka <235736@duan.edu.ua>
2025-08-25 01:02:51 +0300

    24.08

--- 21.08/index.php
+++ 21.08/index.php
@@ -137,3 +137,37 @@ function hello4()
 var_dump($x && hello4()||true);
 echo "<br>";
 echo "<br>";
+$x = 6; //110
+$y = 3; //011
+
+var_dump(~$x); //-7
+echo "<br>";
+var_dump(~$x&$y); //1
+echo "<br>";
+var_dump($x&$y);//2
+echo "<br>";
+var_dump($x|$y);//7
+echo "<br>";
+var_dump($x^$y);//5
+echo "<br>";
+var_dump($x<<$y);//110000 // 48
+echo "<br>";
+var_dump($x>>$y);//0
+echo "<br>";
+var_dump(~$x|$y);
+echo "<br>";
+var_dump(~$x<<60);
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+$x = ["A", "B", "C"];
+$y = ["D", "E", "F"];
+$z = $x+$y;
+var_dump($z);
+echo "<br>";
+echo "<br>";
+$x = ["A", "B", "C"];
+$y = ["D", "E", "F","G","J"];
+$z = $x+$y;
+var_dump($z);
